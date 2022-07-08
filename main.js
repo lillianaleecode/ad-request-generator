332b65375c749087a41aa871ee9e1a667d245aff
lillianaleecode <lilliana.lee@code.berlin>
2022-07-08 16:05:03 +0200

    toggle show and hide parameter´s option

--- main.js
+++ main.js
@@ -4,3 +4,33 @@ toggle.addEventListener('click', () => {
     const onOff = toggle.parentNode.querySelector('.onoff')
     onOff.textContent = toggle.checked ? 'ON' : 'OFF'
 })
+
+//
+
+let toggleShow = document.querySelector(".toggle-1");
+let isShow = true;
+
+function showHide1(){
+    isShow = !isShow;
+    toggleShow.classList.toggle("hide", isShow);
+}
+
+
+//
+
+let toggleShow2 = document.querySelector(".toggle-2");
+let isShow2 = true;
+
+function showHide2(){
+    isShow2 = !isShow2;
+    toggleShow2.classList.toggle("hide", isShow2);
+}
+
+//
+let toggleShow3 = document.querySelector(".toggle-3");
+let isShow3 = true;
+
+function showHide3(){
+    isShow3 = !isShow3;
+    toggleShow3.classList.toggle("hide", isShow3);
+}
