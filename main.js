ac3424a297c8cbe1d6c42575786753e3aafad4fc
lillianaleecode <lilliana.lee@code.berlin>
2022-07-18 12:29:16 +0200

    progress. adding new params

--- main.js
+++ main.js
@@ -17,6 +17,46 @@ toggle.addEventListener('click', () => {
 
 //
 
+let toggleShowPtvSsptv = document.querySelector(".toggle-ptv-ssptv");
+let isShowPtvSsptv = true;
+
+function showHidePtvSsptv(){
+    isShowPtvSsptv = !isShowPtvSsptv;
+    toggleShowPtvSsptv.classList.toggle("hide", isShowPtvSsptv);
+}
+
+//
+let toggleShowId = document.querySelector(".toggle-Id");
+let isShowId = true;
+
+function showHideId(){
+    isShowId = !isShowId;
+    toggleShowId.classList.toggle("hide", isShowId);
+}
+
+
+//
+let toggleShowInvCode = document.querySelector(".toggle-InvCode");
+let isShowInvCode = true;
+
+function showHideInvCode(){
+    isShowInvCode = !isShowInvCode;
+    toggleShowInvCode.classList.toggle("hide", isShowInvCode);
+}
+
+//
+
+
+let toggleShowMember = document.querySelector(".toggle-member");
+let isShowMember = true;
+
+function showHideMember(){
+    isShowMember = !isShowMember;
+    toggleShowMember.classList.toggle("hide", isShowMember);
+}
+
+//
+
 let toggleShow = document.querySelector(".toggle-1");
 let isShow = true;
 
@@ -28,12 +68,12 @@ function showHide1(){
 
 //
 
-let toggleShow2 = document.querySelector(".toggle-2");
-let isShow2 = true;
+let toggleShowvplaybackmethod = document.querySelector(".toggle-vplaybackmethod");
+let isShowvplaybackmethod = true;
 
-function showHide2(){
-    isShow2 = !isShow2;
-    toggleShow2.classList.toggle("hide", isShow2);
+function showHidevplaybackmethod(){
+    isShowvplaybackmethod = !isShowvplaybackmethod;
+    toggleShowvplaybackmethod.classList.toggle("hide", isShowvplaybackmethod);
 }
 
 //
@@ -44,3 +84,14 @@ function showHide3(){
     isShow3 = !isShow3;
     toggleShow3.classList.toggle("hide", isShow3);
 }
+
+//submit button: get form data after the submit button
+
+function generateData(){
+    Array.from(document.querySelectorAll('#generatorForm')).reduce((acc, input) =>({
+        ...acc, [input.id]: input.value }), {})
+    
+        console.log(Array.from(document.querySelectorAll('#generatorForm')).reduce((acc, input) =>({
+            ...acc, [input.id]: input.value }), {})
+        )
+    }
