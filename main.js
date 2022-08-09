272260d0fc1254e56180b8080db9f9946d083c7d
lillianaleecode <lilliana.lee@code.berlin>
2022-08-09 17:13:10 +0200

    clean code dispensables. unneeded whose absence would make the code cleaner. in progress

--- main.js
+++ main.js
@@ -9,19 +9,18 @@ toggle.addEventListener('click', () => {
 })
 
 //
+idtoggle = document.getElementById("idtoggle");
+idOnOff = document.getElementById("idOnOff");
+invCodetoggle = document.getElementById("invCodetoggle");
+invCodeOnOff = document.getElementById("invCodeOnOff");
+invcode = document.getElementById("invcode");
+idparam = document.getElementById("idparam");
+
 let toggleShowId = document.querySelector(".toggle-Id");
-let isShowId = true;
-
 function showHideId(){
-    isShowId = !isShowId;
-    toggleShowId.classList.toggle("hide", isShowId);
+    toggleShowId.classList.toggle("hide");
 
-    idtoggle = document.getElementById("idtoggle");
-    idOnOff = document.getElementById("idOnOff");
-    invCodetoggle = document.getElementById("invCodetoggle");
-    invCodeOnOff = document.getElementById("invCodeOnOff");
-    invcode = document.getElementById("invcode");
-    idparam = document.getElementById("idparam");
+    
 
  //if toggle is in show:
     if (! document.querySelector(".toggle-Id").classList.contains("hide")){
@@ -42,40 +41,18 @@ function showHideId(){
     if (! document.getElementById("idparam") && ! document.getElementById("idparam").value) {
         invCodetoggle.removeAttribute('disabled')
     }
-  
-   
-    // if (idOnOff.textContent = "ON"){
-    //     invCodetoggle.setAttribute('disabled', '');
-    // }
-    // if(idOnOff.textContent = "OFF"){
-    //     idparam.value = "";
-    //     invCodetoggle.setAttribute('disabled', '');
-    // }
-    // if (idOnOff.textContent = "OFF" && invcode.textContent != ""){
-    //     idtoggle.removeAttribute('disabled');
-    // }
-
-
-
-    
     
 }
 
 
 //
 let toggleShowInvCode = document.querySelector(".toggle-InvCode");
-let isShowInvCode = true;
 
 function showHideInvCode(){
-    isShowInvCode = !isShowInvCode;
-    toggleShowInvCode.classList.toggle("hide", isShowInvCode);
 
-    idtoggle = document.getElementById("idtoggle");
-    idOnOff = document.getElementById("idOnOff");
-    invCodetoggle = document.getElementById("invCodetoggle");
-    invCodeOnOff = document.getElementById("invCodeOnOff");
-    invcode = document.getElementById("invcode");
-    idparam = document.getElementById("idparam");
+    toggleShowInvCode.classList.toggle("hide");
+
+    
 
      //if toggle is in show:
     if (! document.querySelector(".toggle-InvCode").classList.contains("hide")){
@@ -111,123 +88,79 @@ function showHideInvCode(){
 
 }
 
-//
-let toggleShowMember = document.querySelector(".toggle-member");
-let isShowMember = true;
-
-function showHideMember(){
-    isShowMember = !isShowMember;
-    toggleShowMember.classList.toggle("hide", isShowMember);
-}
 
 //
-let toggleShow = document.querySelector(".toggle-1");
-let isShow = true;
-
+let toggleShow = document.querySelector(".toggle-gdpr");
 function showHide1(){
-    isShow = !isShow;
-    toggleShow.classList.toggle("hide", isShow);
+    toggleShow.classList.toggle("hide");
 }
 
 
 //
 let toggleShowvplaybackmethod = document.querySelector(".toggle-vplaybackmethod");
-let isShowvplaybackmethod = true;
-
 function showHidevplaybackmethod(){
-    isShowvplaybackmethod = !isShowvplaybackmethod;
-    toggleShowvplaybackmethod.classList.toggle("hide", isShowvplaybackmethod);
+    toggleShowvplaybackmethod.classList.toggle("hide");
 
 }
 
 //
 let toggleShowvwidth = document.querySelector(".toggle-vwidth");
-let isShowvwidth = true;
-
 function showHideVwidth(){
-    isShowvwidth = !isShowvwidth;
-    toggleShowvwidth.classList.toggle("hide", isShowvwidth);
+    toggleShowvwidth.classList.toggle("hide");
 }
 
 //
 let toggleShowvheight = document.querySelector(".toggle-vheight");
-let isShowvheight = true;
-
 function showHideVheight(){
-    isShowvheight = !isShowvheight;
-    toggleShowvheight.classList.toggle("hide", isShowvheight);
+    toggleShowvheight.classList.toggle("hide");
 }
 
 //
 let toggleGdprConsent = document.querySelector(".toggle-gdpr_consent");
-let isGdprConsent = true;
-
 function showHideGdprConsent(){
-    isGdprConsent = !isGdprConsent;
-    toggleGdprConsent.classList.toggle("hide", isGdprConsent);
+    toggleGdprConsent.classList.toggle("hide");
 }
 
 //
 let toggleReferrer = document.querySelector(".toggle-referrer");
-let isReferrer = true;
-
 function showHideReferrer(){
-    isReferrer = !isReferrer;
-    toggleReferrer.classList.toggle("hide", isReferrer);
+    toggleReferrer.classList.toggle("hide");
 }
 
 //
 let toggleAppid = document.querySelector(".toggle-appid");
-let isAppid = true;
-
 function showHideAppid(){
-    isAppid = !isAppid;
-    toggleAppid.classList.toggle("hide", isAppid);
+    toggleAppid.classList.toggle("hide");
 }
 
 //
 let toggleIfa = document.querySelector(".toggle-ifa");
-let isIfa = true;
-
 function showHideIfa(){
-    isIfa = !isIfa;
-    toggleIfa.classList.toggle("hide", isIfa);
+    toggleIfa.classList.toggle("hide");
 }
 
 //
 let toggleIp = document.querySelector(".toggle-ip");
-let isIp = true;
-
 function showHideIp(){
-    isIp = !isIp;
-    toggleIp.classList.toggle("hide", isIp);
+    toggleIp.classList.toggle("hide");
 }
 
 //
 let toggleUa = document.querySelector(".toggle-ua");
-let isUa = true;
-
 function showHideUa(){
-    isUa = !isUa;
-    toggleUa.classList.toggle("hide", isUa);
+    toggleUa.classList.toggle("hide");
 }
 
 //
 let toggleIfaType = document.querySelector(".toggle-ifa_type");
-let isIfaType = true;
-
-function showHideIfaType(){
-    isIfaType = !isIfaType;
-    toggleIfaType.classList.toggle("hide", isIfaType);
+function showHideIfaType(){ 
+    toggleIfaType.classList.toggle("hide");
 }
 
 //
 let toggleDescription = document.querySelector(".toggle-description");
-let isDescription = true;
-
 function showHideDescription(){
-    isDescription = !isDescription;
-    toggleDescription.classList.toggle("hide", isDescription);
+    toggleDescription.classList.toggle("hide");
 }
 
 //concatenate
